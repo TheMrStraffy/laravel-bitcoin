d2ca634be5901a876285909192a3c38406d5fdc7
Giorgio Perez <giorgiop62@gmail.com>
2023-01-16 16:41:33 +0100

    [partials view]

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BitcoinController::class, 'index'])->name('home');
 
-Route::resources('bitcoins',BitcoinController::class);
+Route::resource('bitcoins', BitcoinController::class);
