036f50fd8cadac77a84e4375ee66662fd6789c95
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-16 18:31:36 +0100

    [fixed] edit route & old values

--- resources/views/bitcoin/edit.blade.php
+++ resources/views/bitcoin/edit.blade.php
@@ -3,32 +3,32 @@
 @section('content')
 <div class="container">
     <a class="btn btn-primary" href="{{route('bitcoins.index')}}">Go Back</a>
-    <form action="{{route('bitcoins.update')}}" method="POST" >
+    <form action="{{route('bitcoins.update', $bitcoin)}}" method="POST" >
         @csrf
         @method('PUT')
         <div class="mb-3">
           <label for="date" class="form-label" >Data</label>
-          <input type="date" class="form-control" value="{{old($bitcoin->date)}}" id="date" name="date">
+          <input type="date" class="form-control" value="{{old('date', $bitcoin->date)}}" id="date" name="date">
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="text" class="form-control" value="{{old($bitcoin->price)}}" id="price" name="price">
+            <input type="text" class="form-control" value="{{old('price',$bitcoin->price)}}" id="price" name="price">
         </div>
         <div class="mb-3">
             <label for="open" class="form-label">Apertura</label>
-            <input type="text" class="form-control" value="{{old($bitcoin->open)}}" id="open" name="open">
+            <input type="text" class="form-control" value="{{old('open',$bitcoin->open)}}" id="open" name="open">
         </div>
         <div class="mb-3">
             <label for="high" class="form-label">high</label>
-            <input type="text" class="form-control" value="{{old($bitcoin->high)}}" id="high" name="high">
+            <input type="text" class="form-control" value="{{old('high',$bitcoin->high)}}" id="high" name="high">
         </div>
         <div class="mb-3">
             <label for="change" class="form-label">Cambio</label>
-            <input type="text" class="form-control" value="{{old($bitcoin->change)}}" id="change" name="change">
+            <input type="text" class="form-control" value="{{old('change',$bitcoin->change)}}" id="change" name="change">
         </div>
         <div class="mb-3">
             <label for="volume" class="form-label">Volume</label>
-            <input type="text" class="form-control" value="{{old($bitcoin->volume)}}" id="volume" name="volume">
+            <input type="text" class="form-control" value="{{old('volume',$bitcoin->volume)}}" id="volume" name="volume">
         </div>
 
         <button class="submit">INVIA</button>
