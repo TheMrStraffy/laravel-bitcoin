010116bf3a61aad88867422dd02057ca499a535d
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-16 15:26:17 +0100

    [added] migration and seeder

---
+++ database/migrations/2023_01_16_141848_create_bitcoins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitcoins', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->float('price',8,1);
+            $table->float('open',8,1);
+            $table->float('high',8,1);
+            $table->float('change',3,1);
+            $table->string('volume');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitcoins');
+    }
+};
